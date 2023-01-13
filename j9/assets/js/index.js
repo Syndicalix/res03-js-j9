91738aaa4c912263fddeb28d847fb41dcec24c75
Syndicalix <sebastienspeich@ip-172-31-18-170.eu-west-3.compute.internal>
2023-01-13 14:14:39 +0000

    pourquoi moi :(

--- j9/assets/js/index.js
+++ j9/assets/js/index.js
@@ -1,22 +1,25 @@
 import { Form } from "./Form.js";
 import { Field } from "./Field.js";
 
-let firstname = document.getElementById("firstname");
-let firstnameField = new TextField(firstname);
-firstnameField.name = firstname.getAttribute("name");
+window.addEventListener("DOMContentLoaded", function() {
+    
+    let firstname = document.getElementById("firstname");
+    let firstnameField = new TextField(firstname);
+    firstnameField.name = firstname.getAttribute("name");
 
-let lastname = document.getElementById("lastname");
-let lastnameField = new TextField(lastname);
-lastnameField.name = lastname.getAttribute('name');
+    let lastname = document.getElementById("lastname");
+    let lastnameField = new TextField(lastname);
+    lastnameField.name = lastname.getAttribute('name');
 
-let email = document.getElementById("email");
-let emailField = new EmailField(email);
-emailField.name = email.getAttribute("email");
+    let email = document.getElementById("email");
+    let emailField = new EmailField(email);
+    emailField.name = email.getAttribute("email");
 
-let password = document.getElementById("password");
-let passwordField = new PasswordField(password);
-passwordField.name = password.getAttribute("password");
+    let password = document.getElementById("password");
+    let passwordField = new PasswordField(password);
+    passwordField.name = password.getAttribute("password");
 
-let confirmpassword = document.getElementById("confirm-password");
-let confirmpasswordField = new PasswordField(confirmpassword);
-confirmpasswordField.name = PasswordField("confirm-password");
+    let confirmpassword = document.getElementById("confirm-password");
+    let confirmpasswordField = new PasswordField(confirmpassword);
+    confirmpasswordField.name = PasswordField("confirm-password");
+});
